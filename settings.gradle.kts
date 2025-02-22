faae070fa26afe6be73079b60e540df443c7539f
llsdah <77381506+llsdah@users.noreply.github.com>
2025-02-22 18:14:57 +0900

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ include("algorithm")
 include("zerobase-3rd")
 include("zerobase-3rd:data-structure-and-algorithm")
 findProject(":zerobase-3rd:data-structure-and-algorithm")?.name = "data-structure-and-algorithm"
+include("fastcampus")
